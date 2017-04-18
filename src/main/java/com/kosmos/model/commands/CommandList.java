5376b514ea5ea95ac77f32136f649f70621eb83b
KONSTANTIN <konstantin.turchenko@gmail.com>
2017-04-18 21:44:55 +0600

    refactoring

--- src/main/java/com/kosmos/model/commands/CommandList.java
+++ src/main/java/com/kosmos/model/commands/CommandList.java
@@ -3,6 +3,10 @@ package com.kosmos.model.commands;
 
 import com.kosmos.controller.handler.GetExchange;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CommandList {
 
     public static final String COMMAND_HELP = "/help";
@@ -18,17 +22,27 @@ public class CommandList {
     public static final String AUD = "AUD";
     public static final String CNY = "CNY";
 
-    public static final String DEFAULT_TEXT = "на данный момент, я мало что умею, но мы с моим создателем \n " +
-            "работаем над моей производительность и способностями";
+    public static final List<String> RUB_LIST = Arrays.asList("Рубль", "рубль", "копейки", "RUB");
+    public static final List<String> USD_LIST = Arrays.asList("доллар", "бакс", "зелень", "USD");
+    public static final List<String> EUR_LIST = Arrays.asList("евро", "eur", "EUR", "Евро");
+    public static final List<String> AUD_LIST = Arrays.asList("адоллар", "австралийский", "AUD");
+
+
+    public static final String DEFAULT_TEXT =
+            "на данный момент, я мало что умею, но мы с моим создателем \n " +
+                    "работаем над моей производительность и способностями";
 
     public static final String HELP_TEXT =
             "Данный бот создан с целью изучения языка програмирования Java \n" +
                     "и узучения TelegramBot \n" +
                     "для начала работы введите команду  /commands";
 
-    public static final String COMMANDS_TEXT = COMMAND_COMMANDS + " show all commands \n"
-            + COMMAND_WEATHER + " show weather in Almaty \n"
-            + COMMAND_HELP + " help \n"
-            + COMMAND_NEXT_BASH + " рандомно выводит любую цитату, для начала работы необходимо запустить команду \n";
+    public static final String COMMANDS_TEXT =
+            COMMAND_COMMANDS + " show all commands \n"
+                    + COMMAND_WEATHER + " show weather in Almaty \n"
+                    + COMMAND_HELP + " help \n"
+                    + COMMAND_NEXT_BASH + " рандомно выводит любую цитату, для начала работы необходимо запустить команду \n" +
+                    "если вы хотите узнать курс валют по отношению к ТЕНГЕ просто введите название валюты";
+
 
 }
